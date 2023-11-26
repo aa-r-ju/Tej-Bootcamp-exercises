720ec13b4fdb62c9ddd405e6597f03f909938c21
haster-tabloids <sudeshmate35@gmail.com>
2023-11-26 21:58:32 +0545

    feat:completed part1-a 1.1 exercise

---
+++ part1/src/Component/Content.jsx
@@ -0,0 +1,10 @@
+const Content = (props) => {
+    return(
+      <div> 
+       <p> {props.part1[0]} {props.exercises1[0]}</p>
+       <p> {props.part1[1]} {props.exercises1[1]}</p>
+       <p> {props.part1[2]} {props.exercises1[2]}</p>
+      </div>
+    )
+  }
+  export default Content;
---
+++ part1/src/Component/Total.jsx
@@ -0,0 +1,10 @@
+
+const Total = (props) => {
+    return(
+      <div>
+      <p> Number of exercises is {props.total[0] + props.total[1] + props.total[2]}</p>
+      </div>
+    )
+  }
+
+  export default Total;
