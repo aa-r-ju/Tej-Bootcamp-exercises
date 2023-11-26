5cdbefb00276ba2d7aa383068d92d10f111bcfe9
haster-tabloids <sudeshmate35@gmail.com>
2023-11-26 22:42:41 +0545

    fix:component Part.jsx

--- part1/src/Component/Part.jsx
+++ part1/src/Component/Part.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Part = (props) => {
+const Part = ({chapter, number}) => {
     return(
         <div> 
-        <p> {props.chapter.part1[0]} {props.chapter.exercises1[0]}</p>
-        <p> {props.chapter.part1[1]} {props.chapter.exercises1[1]}</p>
-        <p> {props.chapter.part1[2]} {props.chapter.exercises1[2]}</p>
+        <p> {chapter} {number}</p>
        </div>
  
     )
