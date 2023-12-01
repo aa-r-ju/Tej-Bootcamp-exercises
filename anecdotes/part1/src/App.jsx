e7aa7d95ddb6d1b6043e98440220b7802a0e4de3
aa-r-ju <aarju3447@gmail.com>
2023-12-01 16:51:54 +0545

    feat:completed all part1-d 1.14 exercise

--- anecdotes/part1/src/App.jsx
+++ anecdotes/part1/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const anecdotes = [
@@ -9,31 +9,41 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is the same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast is to go well.'
-  ]
+    'The only way to go fast is to go well.',
+  ];
 
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
   const [selected, setSelected] = useState(0);
+  const [mostVotedIndex, setMostVotedIndex] = useState(0);
 
   const handleRandomAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex);
-  }
+  };
 
   const handleVote = () => {
     const newVotes = [...votes];
     newVotes[selected] += 1;
     setVotes(newVotes);
-  }
+
+    // Update mostVotedIndex if the newly voted anecdote has more votes
+    if (newVotes[selected] > newVotes[mostVotedIndex]) {
+      setMostVotedIndex(selected);
+    }
+  };
 
   return (
     <>
+      <h2>Anecdote of the day</h2>
       <p>{anecdotes[selected]}</p>
       <p>Has {votes[selected]} votes</p>
       <button onClick={handleVote}>Vote</button>
       <button onClick={handleRandomAnecdote}>Next Anecdote</button>
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[mostVotedIndex]}</p>
+      <p>Has {votes[mostVotedIndex]} votes</p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
