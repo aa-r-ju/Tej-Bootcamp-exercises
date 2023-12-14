bde556736f48f33d5f1c6c58c0f47b5eec76609a
aa-r-ju <aarju3447@gmail.com>
2023-12-14 15:06:02 +0545

    feat:completed part2 2.18 exercise

--- part-2/Phonebook/src/App.jsx
+++ part-2/Phonebook/src/App.jsx
@@ -1,31 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import noteService from "./note"
+import noteService from "./note";
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
-
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newVal, setNewVal] = useState('');
   const [searchVal, setSearchVal] = useState('');
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
-    noteService
-    .getAll()
-    .then(initialNotes => {
-      console.log(initialNotes,"aarju,,,")
-      setPersons(initialNotes)
+    noteService.getAll()
+      .then(initialNotes => {
+        setPersons(initialNotes);
+      })
+      .catch(error => {
+        showErrorNotification('Error fetching data from the server.');
+        console.error('Error fetching data:', error);
       });
   }, []);
+
+  const showSuccessNotification = (message) => {
+    setNotification({ type: 'success', message });
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000); // 5000 milliseconds (5 seconds)
+  };
+
+  const showErrorNotification = (message) => {
+    setNotification({ type: 'error', message });
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000); // 5000 milliseconds (5 seconds)
+  };
 
   const addNote = (event) => {
     event.preventDefault();
 
     if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook.`);
+      showErrorNotification(`${newName} is already added to the phonebook.`);
       return;
     }
 
@@ -34,34 +49,40 @@ const App = () => {
     noteService.create(newPerson)
      .then(value => {
        setPersons([...persons,value]);
-
-      setNewName('');
-      setNewVal('');
-    }).catch (error => {
-      console.error('Error adding person:', error);
-
+       setNewName('');
+       setNewVal('');
+       showSuccessNotification(`${newPerson.name} added to the phonebook.`);
      })
-     
+     .catch(error => {
+       showErrorNotification('Error adding person. Please try again.');
+       console.error('Error adding person:', error);
+     });
+  };
+
+  const handleDelete = (id) => {
+    const personToDelete = persons.find((person) => person.id === id);
+
+    const confirmDeletion = window.confirm(`Delete ${personToDelete.name}?`);
+
+    if (confirmDeletion) {
+      noteService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter((person) => person.id !== id));
+          showSuccessNotification(`${personToDelete.name} deleted from the phonebook.`);
+        })
+        .catch((error) => {
+          if (error.response && error.response.status === 404) {
+            showErrorNotification(`${personToDelete.name} not found. The person may have been deleted.`);
+          } else {
+            showErrorNotification('Error deleting person. Please try again.');
+          }
+          console.error('Error deleting person:', error);
+        });
     }
+  };
 
-    const handleDelete = (id) => {
-      const personToDelete = persons.find((person) => person.id === id);
-      
-      const confirmDeletion = window.confirm(`Delete ${personToDelete.name}?`);
-      
-      if (confirmDeletion) {
-        noteService
-          .remove(id)
-          .then(() => {
-            setPersons(persons.filter((person) => person.id !== id));
-          })
-          .catch((error) => {
-            console.error('Error deleting person:', error);
-          });
-      }
-    };
-
-    const existingPerson = persons.find((person) => person.name === newName);
+  const existingPerson = persons.find((person) => person.name === newName);
 
   if (existingPerson) {
     const confirmUpdate = window.confirm(
@@ -75,16 +96,18 @@ const App = () => {
         .update(existingPerson.id, updatedPerson)
         .then((updatedPerson) => {
           setPersons(persons.map((person) => (person.id !== updatedPerson.id ? person : updatedPerson)));
+          showSuccessNotification(`${updatedPerson.name}'s number updated successfully.`);
         })
         .catch((error) => {
+          if (error.response && error.response.status === 404) {
+            showErrorNotification(`${updatedPerson.name} not found. The person may have been deleted.`);
+          } else {
+            showErrorNotification('Error updating person. Please try again.');
+          }
           console.error('Error updating person:', error);
         });
     }
-  } else {
-    const newPerson = { name: newName, number: newVal };
   }
-    
-
 
   const handleAddNote = (event) => {
     setNewName(event.target.value);
@@ -102,12 +125,15 @@ const App = () => {
     person.name.toLowerCase().includes(searchVal.toLowerCase())
   );
 
-
- 
-
   return (
     <div>
       <h2>Phonebook</h2>
+
+      {notification && (
+        <div style={{ color: notification.type === 'error' ? 'red' : 'green', border: '1px solid', padding: '10px', marginBottom: '10px' }}>
+          {notification.message}
+        </div>
+      )}
 
       <Filter handleSearch={handleSearch} />
 
