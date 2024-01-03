04445a5ace86e0f3b43e1f5a257fd15052c706b9
aa-r-ju <aarju3447@gmail.com>
2024-01-03 15:51:01 +0545

    feat:completed part4 4.5 exercise

--- part-4/backend-server/utils/list_helper.js
+++ part-4/backend-server/utils/list_helper.js
@@ -2,16 +2,39 @@ const dummy = (blogs) => {
     return 1
   }
   
-  
 
-const  totalLikes = (listWithOneBlog) => {
+  const totalLikes = (listWithOneBlog) => {
     let newVal = 0;
     if(listWithOneBlog.length === 1) {
         newVal = listWithOneBlog[0].likes
     }
     return newVal
-}
-
-module.exports = {
-    dummy, totalLikes
   }
+
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null;
+    }
+  
+    let favorite = blogs[0];
+  
+    for (let i = 1; i < blogs.length; i++) {
+      if (blogs[i].likes > favorite.likes) {
+        favorite = blogs[i]; 
+      }
+    }
+  
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    };
+  };
+  
+
+  
+
+  module.exports = {
+    dummy,totalLikes,favoriteBlog
+  }
+  
