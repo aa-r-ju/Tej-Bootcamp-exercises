30057ffebd9f8e3daaae3cfa84ce15d32c52968a
aa-r-ju <aarju3447@gmail.com>
2024-01-03 11:55:43 +0545

    feat:completed part4 4.3 exercise

---
+++ part-4/backend-server/utils/list_helper.js
@@ -0,0 +1,17 @@
+const dummy = (blogs) => {
+    return 1
+  }
+  
+  
+
+const  totalLikes = (listWithOneBlog) => {
+    if(listWithOneBlog !== 5) {
+        return "there are no match"
+    }
+        return 5
+    
+}
+
+module.exports = {
+    dummy, totalLikes
+  }
