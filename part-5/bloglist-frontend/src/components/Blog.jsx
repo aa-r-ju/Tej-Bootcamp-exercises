b3d79df42b05ceb08f5e419e8c8401dc5b720be9
aa-r-ju <aarju3447@gmail.com>
2024-02-05 15:13:56 +0545

    feat:completed part5 .7 exercise

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
 
-export default Blog
+
+const Blog = ({ blog }) => {
+  const [showData, setShowData] = useState('')
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const togglAble = () => {
+  setShowData(!showData)
+}
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={togglAble}>
+          {showData ? 'Hide' : 'View'}
+        </button>
+      </div>
+      {showData && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            Likes: {blog.likes}{' '}
+            <button onClick={() => console.log('Like button clicked')}>
+              Like
+            </button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Blog;
