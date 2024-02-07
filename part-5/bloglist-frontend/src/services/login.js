b634d8e3ae19e6e3be5604d87d09fa46fd19b01b
aa-r-ju <aarju3447@gmail.com>
2024-02-07 10:45:29 +0545

    feat:completed part5 5.15 exercise

--- part-5/bloglist-frontend/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const baseUrl = "http://localhost:3003/api/login";
-
-const login = async (user) => {
-  let loggedinUser = await axios.post(baseUrl, user);
-  return loggedinUser.data;
-};
-export default { login };
