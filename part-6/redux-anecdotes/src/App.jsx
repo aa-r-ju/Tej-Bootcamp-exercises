2e341792b74e1f77505acb27b030d45ea52cbe7a
aa-r-ju <aarju3447@gmail.com>
2024-02-15 15:39:36 +0545

    feat:completed part6 6.5 exercise

--- part-6/redux-anecdotes/src/App.jsx
+++ part-6/redux-anecdotes/src/App.jsx
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const App = () => {
   const anecdotes = useSelector(state => state);
+
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
+
   const dispatch = useDispatch();
 
   const vote = (id) => {
@@ -16,7 +19,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
