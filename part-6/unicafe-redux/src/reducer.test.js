2db1b7486173639a1df54997b7764ca9ce7e19b9
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-02-27 16:57:00 +0545

    feat:completed part6 6.2 exercise

--- part-6/unicafe-redux/src/reducer.test.js
+++ part-6/unicafe-redux/src/reducer.test.js
@@ -27,7 +27,7 @@ describe("unicafe reducer", () => {
     deepFreeze(state);
     const newState = counterReducer(state, action);
     expect(newState).toEqual({
-      good: 1,
+      good: 5,
       ok: 0,
       bad: 0,
     });
@@ -43,7 +43,7 @@ describe("unicafe reducer", () => {
     const newState = counterReducer(state, action);
     expect(newState).toEqual({
       good: 0,
-      ok: 1,
+      ok: 4,
       bad: 0,
     });
   });
@@ -59,7 +59,7 @@ describe("unicafe reducer", () => {
     expect(newState).toEqual({
       good: 0,
       ok: 0,
-      bad: 1,
+      bad: 3,
     });
   });
 
@@ -68,9 +68,9 @@ describe("unicafe reducer", () => {
       type: "ZERO",
     };
     const state = {
-      good: 3,
-      ok: 2,
-      bad: 1,
+      good: 5,
+      ok: 4,
+      bad: 3,
     };
 
     deepFreeze(state);
