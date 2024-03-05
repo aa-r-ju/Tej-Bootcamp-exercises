eff18a21a640002909ee8b343d857dd78078203c
aa-r-ju <aarju3447@gmail.com>
2024-03-05 16:24:41 +0545

    feat:completed part6 6.11 exedrcise

--- part-6/redux-anecdotes/src/reducers/filterReducer.js
+++ part-6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,19 +1,33 @@
-const filterReducer = (state = "", action) => {
-  switch (action.type) {
-    case "FILTER-ANECDOTE": {
+// const filterReducer = (state = "", action) => {
+//   switch (action.type) {
+//     case "FILTER-ANECDOTE": {
+//       return action.payload;
+//     }
+
+//     default:
+//       return state;
+//   }
+// };
+
+// export const anecdoteFilterFrom = (searchTerm) => {
+//   return {
+//     type: "FILTER-ANECDOTE",
+//     payload: searchTerm,
+//   };
+// };
+
+// export default filterReducer;
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const filterReducer = createSlice({
+  name: "filter",
+  initialState: "",
+  reducers: {
+    anecdoteFilterFrom(state, action) {
       return action.payload;
-    }
-
-    default:
-      return state;
-  }
-};
-
-export const anecdoteFilterFrom = (searchTerm) => {
-  return {
-    type: "FILTER-ANECDOTE",
-    payload: searchTerm,
-  };
-};
-
-export default filterReducer;
+    },
+  },
+});
+export const { anecdoteFilterFrom } = filterReducer.actions;
+export default filterReducer.reducer;
