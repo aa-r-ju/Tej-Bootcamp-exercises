faf6acc00cbb4cfbe437cba5180fcc11cbf15d1d
aa-r-ju <aarju3447@gmail.com>
2024-03-05 17:31:24 +0545

    feat:completed part6 6.13 completed

--- part-6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part-6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,13 +1,22 @@
 import { useDispatch } from "react-redux";
 import { newAnecdote } from "../reducers/anecdoteReducer";
+import {
+  resetNotification,
+  setNotification,
+} from "../reducers/notificationReducer";
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
   const addNew = (event) => {
     event.preventDefault();
-    console.log(event.target.newAnecdote.value);
-    dispatch(newAnecdote(event.target.newAnecdote.value));
+    const content = event.target.newAnecdote.value;
+    console.log(content, "test");
+    dispatch(newAnecdote(content));
     event.target.newAnecdote.value = "";
+    dispatch(setNotification(`You added new anecdote '${content}'`));
+    setTimeout(() => {
+      dispatch(resetNotification());
+    }, 5000);
   };
   return (
     <>
--- part-6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part-6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,14 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
+import {
+  resetNotification,
+  setNotification,
+} from "../reducers/notificationReducer";
 
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) => state.anecdote);
   const filters = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
-  const vote = (id) => {
-    console.log("vote", id);
+  const vote = (anecdote) => {
+    const { id, content } = anecdote;
     dispatch(voteAnecdote(id));
+    dispatch(setNotification(`You voted ${content}`));
+    setTimeout(() => {
+      dispatch(resetNotification());
+    }, 5000);
   };
 
   const ShowAnecdote = filters
@@ -26,7 +34,7 @@ const AnecdoteList = () => {
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => vote(anecdote)}>vote</button>
           </div>
         </div>
       ))}
