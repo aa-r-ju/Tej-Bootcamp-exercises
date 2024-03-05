abfdf4bba38ce8e3d2604f27529e22e3458a5405
aa-r-ju <aarju3447@gmail.com>
2024-03-05 16:03:11 +0545

    feat:completed part6 6.9 exercise where i made the new file filterReducer and refactor them

--- part-6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part-6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { voteAnecdote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) => state.anecdote);
+  const filters = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
   const vote = (id) => {
@@ -10,19 +11,25 @@ const AnecdoteList = () => {
     dispatch(voteAnecdote(id));
   };
 
+  const ShowAnecdote = filters
+    ? anecdotes.filter((anecdote) =>
+        anecdote.content.toLowerCase().includes(filters.toLowerCase())
+      )
+    : anecdotes;
+
+  ShowAnecdote.sort((a, b) => b.votes - a.votes);
+
   return (
     <>
-      {anecdotes
-        .sort((a, b) => b.votes - a.votes)
-        .map((anecdote) => (
-          <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
+      {ShowAnecdote.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
-        ))}
+        </div>
+      ))}
     </>
   );
 };
