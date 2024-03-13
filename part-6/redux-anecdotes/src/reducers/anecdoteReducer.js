0d1a9005698909a3b80a282cc9aea09bb75f31d2
aa-r-ju <aarju3447@gmail.com>
2024-03-13 14:48:06 +0545

    feat:completed part6 6.16 exercise

--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,5 +1,6 @@
 //already arrenged the file with correct path
 import { createSlice } from "@reduxjs/toolkit";
+import { getAnecdotes } from "../services/anecdotes";
 
 const anecdoteReducer = createSlice({
   name: "anecdotes",
@@ -16,12 +17,19 @@ const anecdoteReducer = createSlice({
     newAnecdote(state, action) {
       return state.concat(action.payload);
     },
-    initializeAnecdote(state, action) {
+    setAnecdotes(state, action) {
       return action.payload;
     },
   },
 });
 
-export const { voteAnecdote, newAnecdote, initializeAnecdote } =
+export const initializeAnecdote = () => {
+  return async (dispatch) => {
+    const notes = await getAnecdotes();
+    dispatch(setAnecdotes(notes));
+  };
+};
+
+export const { voteAnecdote, newAnecdote, setAnecdotes } =
   anecdoteReducer.actions;
 export default anecdoteReducer.reducer;
