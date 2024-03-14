79499ca72456cc8ec635fe7d6839eb4a47c3f9e9
aa-r-ju <aarju3447@gmail.com>
2024-03-14 16:37:04 +0545

    feat:completed part6 6.24 exercise

--- part-6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part-6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -6,7 +6,7 @@ import NotificationContext from "../NotificationContext";
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient();
-  const [, contextDispatch] = useContext(NotificationContext);
+  const [notification, contextDispatch] = useContext(NotificationContext);
 
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote,
@@ -22,18 +22,30 @@ const AnecdoteForm = () => {
   const addNew = async (event) => {
     event.preventDefault();
     const content = event.target.newAnecdote.value;
-    const anecdoteToAdd = { content, votes: 0 };
-    newAnecdoteMutation.mutate(anecdoteToAdd);
-    event.target.newAnecdote.value = "";
-    contextDispatch({
-      type: "SET_NOTIFICATION",
-      payload: `anecdote '${content}' is added`,
-    });
-    setTimeout(() => {
+    if (content.length > 5) {
+      const anecdoteToAdd = { content, votes: 0 };
+      newAnecdoteMutation.mutate(anecdoteToAdd);
+      event.target.newAnecdote.value = "";
       contextDispatch({
-        type: "RESET_NOTIFICATION",
+        type: "SET_NOTIFICATION",
+        payload: `anecdote '${content}' is added`,
       });
-    }, 5000);
+      setTimeout(() => {
+        contextDispatch({
+          type: "RESET_NOTIFICATION",
+        });
+      }, 2000);
+    } else {
+      contextDispatch({
+        type: "SET_NOTIFICATION",
+        payload: `too short anecdote, must have length 5 or more`,
+      });
+      setTimeout(() => {
+        contextDispatch({
+          type: "RESET_NOTIFICATION",
+        });
+      }, 5000);
+    }
   };
   return (
     <>
