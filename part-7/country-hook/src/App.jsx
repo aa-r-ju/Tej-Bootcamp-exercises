ec9c12c8a839fd6b67b6db35f807f56794eb60e2
aa-r-ju <aarju3447@gmail.com>
2024-03-20 11:52:12 +0545

    feat:completed part7 7.7 exercise

--- part-7/country-hook/src/App.jsx
+++ part-7/country-hook/src/App.jsx
@@ -1,71 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
-
-  useEffect(() => {})
-
-  return country
-}
+import React, { useState } from "react";
+import { useCountry } from "./hooks/useCountry";
+import { useField } from "./hooks/useField";
 
 const Country = ({ country }) => {
+  console.log(country, "outside useEffect");
+
   if (!country) {
-    return null
+    return null;
   }
-
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div> not found...</div>;
   }
-
   return (
     <div>
-      <h3>{country.data.name} </h3>
+      <h3>{country.data.name.common} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flags.png}
+        height="100"
+        alt={`flag of ${country.data.flags.alt}`}
+      />
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-  const country = useCountry(name)
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
+  console.log(country, "from app component");
 
   const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-  }
-
+    e.preventDefault();
+    setName(nameInput.value);
+  };
   return (
     <div>
       <form onSubmit={fetch}>
+        <p>Find Countriess</p>
         <input {...nameInput} />
-        <button>find</button>
       </form>
-
       <Country country={country} />
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
